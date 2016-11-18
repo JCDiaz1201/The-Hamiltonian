13bd78aa480871438d83965f1d19dc2f20141bc9
JDjedi <JDjedi1201@gmail.com>
2016-11-17 20:33:19 -0600

    Logout functionality still needs work

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 		'/show',	to: 'visitors#show'
   get    	'/login',   to: 'sessions#new'
   post   	'/login',   to: 'sessions#create'
-  delete 	'/logout',  to: 'sessions#destroy'
+  get	 	'/logout',  to: 'sessions#destroy'
 
   resources 		:visitors, only:[:index]
   resources 		:posts
