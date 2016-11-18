6ddcdb6c1b16f409844bc3909732ac270017356c
JDjedi <JDjedi1201@gmail.com>
2016-11-17 22:18:14 -0600

    Completed login/out functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   root 						'visitors#index'
-  get 		'/about',	to: 'visitors#about'
-  get 		'/show',	to: 'visitors#show'
-  get    	'/login',   to: 'sessions#new'
-  post   	'/login',   to: 'sessions#create'
-  get	 	'/logout',  to: 'sessions#destroy'
+  get 			'/about',	to: 'visitors#about'
+  get 			'/show',	to: 'visitors#show'
+  get    		'/login',   to: 'sessions#new'
+  post   		'/login',   to: 'sessions#create'
+  get	 		'/logout',  to: 'sessions#destroy'
 
   resources 		:visitors, only:[:index]
   resources 		:posts
