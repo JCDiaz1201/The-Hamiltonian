0385a6ec0f7236a47b666ca9a52ef1df0f3bd1b4
JDjedi <JDjedi1201@gmail.com>
2017-03-07 22:54:01 -0600

    Added to config for sending email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,6 +86,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: "www.the-hamiltonian.org", :protocol => 'http'} }
+  config.action_mailer.default_url_options = { host: "www.the-hamiltonian.org", :protocol => 'http' }
   config.action_mailer.perform_deliveries = true
 end
