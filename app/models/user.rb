e5a4e91222dd6e812bd962fa1900acffe60cc1a9
JDjedi <jcdiaz1201@gmail.com>
2018-04-27 17:19:44 -0500

    Updated about section

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,14 +7,16 @@ class User < ActiveRecord::Base
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	before_save { self.email = self.email.downcase } #ensure that unique emails only get saved
 
-	validates :pseudonym, presence: true,
-				length: { maximum: 50}
+	validates :pseudonym, presence: true, length: { maximum: 50}
+
 	validates :email, presence: true,
 				uniqueness: { case_sensitive: false },
 				length: { maximum: 250},
 				format: { with: VALID_EMAIL_REGEX }
-	has_secure_password
+
 	validates :password, presence: true, length: { minimum: 7 }
+	has_secure_password
+	
 
 	def email_activate
     self.email_confirmed = true
